3f8ba6b8a6d4a6fee285bb1f8ddb71f4cd6cb5d9
Sherry Zhao <sherrytxz06@gmail.com>
2022-11-04 02:44:57 -0500

    content test passed

--- react-tutorial/cypress/integration/App.spec.js
+++ react-tutorial/cypress/integration/App.spec.js
@@ -6,4 +6,8 @@ describe('Test App', () => {
         cy.visit('/');
     });
 
+    it('opens with Fall CS courses', () => {
+        cy.visit('/');
+        cy.get('[data-cy=course]').should('contain', 'Fall CS');
+    });
 });
--- react-tutorial/src/components/CourseCard.js
+++ react-tutorial/src/components/CourseCard.js
@@ -3,23 +3,27 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { useNavigate } from "react-router-dom";
 import { useProfile } from '../utilities/profile';
 
-const CourseCard = ({id, title, name, time, selected, toggleSelected, confiltedCourses}) => {
+const CourseCard = ({ id, title, name, time, selected, toggleSelected, confiltedCourses }) => {
 
     const [{ isAdmin }] = useProfile();
 
-    let navigate = useNavigate(); 
-    const gotoCourseForm = (id) =>{ 
-        let path = `/courses/${id}`; 
+    let navigate = useNavigate();
+    const gotoCourseForm = (id) => {
+        let path = `/courses/${id}`;
         navigate(path,
-            {state: {
-                id: id,
-                code: title,
-                title: name,
-                meets: time
-            }});
+            {
+                state: {
+                    id: id,
+                    code: title,
+                    title: name,
+                    meets: time
+                }
+            });
     }
 
-    return <div className={`card m-1 p-2 ${selected.includes(id) ? 'selected' : ''} ${confiltedCourses.includes(id) ? 'conflicted' : ''}`} onClick={() => toggleSelected(id)}>
+    return <div className={`card m-1 p-2 ${selected.includes(id) ? 'selected' : ''} ${confiltedCourses.includes(id) ? 'conflicted' : ''}`}
+                data-cy="course"
+                onClick={() => toggleSelected(id)}>
         <div className="card-body">
             {isAdmin && <button type="button" className="btn btn-link edit-btn" onClick={() => gotoCourseForm(id)}>
                 <i className="bi bi-pencil-square"></i>
